499d470a698165accf88fba49b73f585a84e41ab
vuckoviic <vuckovica64@gmail.com>
2023-08-18 15:49:21 +0200

    Fixed bug
    function was creating one more div, so I added -1 in for loop

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ sizeBtn.addEventListener('click' , function() {
     let layoutInput = prompt('Enter the lenght of one side of your sketchpad:');
     let layoutNumber = parseFloat(layoutInput);
 
-    for (let i = 0; i <= layoutNumber*layoutNumber; i++){
+    if (layoutInput > 100){
+        alert('Error! You entered too big number!')
+    }
+
+    for (let i = 0; i <= layoutNumber*layoutNumber-1; i++){
         let square = document.createElement('div');
         square.classList.add('square');
+
+        let squareSize = 700 / layoutNumber;
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
         container.appendChild(square);
     }
 });
