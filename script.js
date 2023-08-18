1d843613c525194d13a807153df35c247fe05c9f
vuckoviic <vuckovica64@gmail.com>
2023-08-18 15:02:56 +0200

    Made JS working
    divs are being appended to container

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const container = document.querySelector('container');
+const container = document.querySelector('.container');
 
 const sizeBtn = document.getElementById('size');
 sizeBtn.addEventListener('click' , function() {
-    let layout = prompt('Enter the lenght of one side of your sketchpad:');
-    console.log(layout);
+    let layoutInput = prompt('Enter the lenght of one side of your sketchpad:');
+    let layoutNumber = parseFloat(layoutInput);
+
+    for (let i = 0; i <= layoutNumber; i++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+    }
+
 });
